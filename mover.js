9dbcb6a67601099c2240e5c021d52ac340bdb1bf
daryl <tsdaryl@gmail.com>
2021-04-06 17:53:01 -0700

    create collateFolders and readFolders functions

--- mover.js
+++ mover.js
@@ -1,2 +1,36 @@
-const fs = require('fs');
+const { readdirSync } = require('fs');
 
+const BASE_DIR = './target';
+
+const fileTypes = [
+  'jpg',
+];
+
+
+const readFolders = (targetFolder) => {
+  folderList = [];
+  for (const folderName of readdirSync(targetFolder)) {
+    folderList.push(folderName);
+  }
+  return folderList;
+};
+
+
+const collateFolders = () => {
+  const folderList = {};
+  let folderLevel = 1;
+
+  for (const folderName of readdirSync(BASE_DIR)) {
+    const levelKey = `level-${folderLevel}`;
+    if (folderList[levelKey]) {
+      folderList[levelKey].push(folderName)
+    } else {
+      folderList[levelKey] = [];
+      folderList[levelKey].push(folderName)
+    }
+  }
+  folderLevel ++;
+  return folderList;
+}
+
+console.log(readFolders());
