22ddc3bd2d9f8c192175778f0bfef1b5aae7bbcd
daryl <tsdaryl@gmail.com>
2021-04-06 21:18:28 -0700

    fix error from files with no extensions

--- mover.js
+++ mover.js
@@ -1,6 +1,6 @@
-const { readdirSync } = require('fs');
+const { readdirSync, lstatSync } = require('fs');
 
-const BASE_DIR = './sampleDir';
+const BASE_DIR = 'target';
 
 const fileTypes = [
   'jpg',
@@ -12,20 +12,29 @@ const dirMap = {};
 const extract = (targetDir) => {
   const breadCrumbs = targetDir.split('/');
   const currentDir = breadCrumbs[breadCrumbs.length - 1];
-  console.log(`Currently analyzing: "${targetDir}"...`)
+
+  let dashes = '-- '.repeat(breadCrumbs.length);
+  console.log(`${dashes}Currently analyzing: "${targetDir}"...`)
+
+  if (!lstatSync(targetDir).isDirectory()) {
+    console.log(dashes + 'Not a directory');
+    return;
+  }
   const subdirs = readdirSync(targetDir);
   
   if (subdirs.length === 0) {
-    console.log(`"${targetDir}" has no subdirectories`)
+    console.log(`${dashes}"${targetDir}" has no subdirectories`)
+    // for (const level of breadCrumbs.splice(1)) {
+    // }
     return {
       [currentDir]: []
     }
   }
 
-  console.log(`Subdirectories for "${targetDir}":`)
+  console.log(`${dashes}Contents for "${targetDir}":`)
   for (const dir of subdirs) {
-    console.log('-----' + dir)
-    console.log('-----' + breadCrumbs)
+    console.log(dashes + dir)
+    // console.log('-----' + breadCrumbs)
     extract(targetDir + '/' + dir);
   }
 
