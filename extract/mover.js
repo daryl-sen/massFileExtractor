8a4eaed26d8d392c6c72fe715bc1cd1c72336717
daryl <tsdaryl@gmail.com>
2021-04-07 14:04:03 -0700

    move constants to index.js for easy reconfig

--- extract/mover.js
+++ extract/mover.js
@@ -1,11 +1,5 @@
 const { readdirSync, lstatSync, rename, existsSync, mkdirSync } = require('fs');
 
-const SOURCE_DIR = 'target';
-const OUTPUT_DIR = 'collated';
-const PRINT      = true; // prints the file structure in the terminal
-const FILE_MODE  = process.argv[2]; // `ONLY` mode to only move the selected file types; `EXCLUDE` mode to move everything except the selected file types
-const FILE_TYPES = process.argv.slice(3); // list all file types separated by a single space, i.e. `.jpg .png .gif`
-
 const extract = (targetDir = SOURCE_DIR) => {
   // create the output directory
   if (!existsSync(OUTPUT_DIR)) {
@@ -30,22 +24,22 @@ const extract = (targetDir = SOURCE_DIR) => {
       newLocation = newLocArray.join('.') + "-" + repeats + "." + fileExt;
     }
     if (repeats) {
-      rename(targetDir, newLocation, () => {console.log(`(Renamed duplicate file name) Moving from "${targetDir}" to "${newLocation}"...`)});
+      rename(targetDir, newLocation, () => {console.log(`(Duplicate name) Moving from "${targetDir}" to "${newLocation}"...`)});
     } else {
       rename(targetDir, newLocation, () => {console.log(`Moving from "${targetDir}" to "${newLocation}"...`)});
     }
     return;
   }
 
+  // work on current target directory
   const subdirs = readdirSync(targetDir);
-  
   if (subdirs.length === 0) {
     return {
       [currentDir]: []
     }
   }
 
-  // PRINT && console.log(`${dashes}/${targetDir} (current dir)`)
+  // work on subdirectories recursively
   for (const dir of subdirs) {
     PRINT && console.log(dashes + dir);
     extract(targetDir + '/' + dir);
