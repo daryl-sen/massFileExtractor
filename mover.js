d8896cff39f73bacc67bce079233892ad4c472ee
daryl <tsdaryl@gmail.com>
2021-04-06 18:10:30 -0700

    restructure to recursive function

--- mover.js
+++ mover.js
@@ -1,36 +1,17 @@
 const { readdirSync } = require('fs');
 
-const BASE_DIR = './target';
+const BASE_DIR = './empty';
 
 const fileTypes = [
   'jpg',
 ];
 
 
-const readFolders = (targetFolder) => {
-  folderList = [];
-  for (const folderName of readdirSync(targetFolder)) {
-    folderList.push(folderName);
+const extract = (targetDir) => {
+  const subdirs = readdirSync(targetDir);
+  for (const dir of subdirs) {
+    extract(dir);
   }
-  return folderList;
+
+  
 };
-
-
-const collateFolders = () => {
-  const folderList = {};
-  let folderLevel = 1;
-
-  for (const folderName of readdirSync(BASE_DIR)) {
-    const levelKey = `level-${folderLevel}`;
-    if (folderList[levelKey]) {
-      folderList[levelKey].push(folderName)
-    } else {
-      folderList[levelKey] = [];
-      folderList[levelKey].push(folderName)
-    }
-  }
-  folderLevel ++;
-  return folderList;
-}
-
-console.log(readFolders());
