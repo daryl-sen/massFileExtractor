9b73ee36b333958bc6c06d4083219c68565cf720
daryl <tsdaryl@gmail.com>
2021-04-06 18:27:37 -0700

    refine structure

--- mover.js
+++ mover.js
@@ -1,17 +1,31 @@
 const { readdirSync } = require('fs');
 
-const BASE_DIR = './empty';
+const BASE_DIR = './sampleDir';
 
 const fileTypes = [
   'jpg',
 ];
 
 
+const dirMap = {};
+
 const extract = (targetDir) => {
+  const breadCrumbs = targetDir.split('/');
+  const currentDir = breadCrumbs[breadCrumbs.length - 1];
+  console.log(breadCrumbs, currentDir);
   const subdirs = readdirSync(targetDir);
-  for (const dir of subdirs) {
-    extract(dir);
+  
+  if (subdirs.length === 0) {
+    return {
+      [currentDir]: []
+    }
   }
 
-  
+  for (const dir of subdirs) {
+    console.log(breadCrumbs)
+    extract(targetDir + '/' + dir);
+  }
+
 };
+
+extract(BASE_DIR)
