33a7008089ac37099330feb7d2f0dbb0b3cdc4ae
daryl <tsdaryl@gmail.com>
2021-04-08 21:35:01 -0700

    fix minor bug with config.json

--- extract/mover.js
+++ extract/mover.js
@@ -3,7 +3,7 @@ const { readdirSync, lstatSync, rename, existsSync, mkdirSync, readFileSync } = 
 const CUSTOM_CONFIG = JSON.parse(readFileSync('./config.json'));
 const SOURCE_DIR = CUSTOM_CONFIG.sourceDir || './target';
 const OUTPUT_DIR = CUSTOM_CONFIG.outputDir || './collated';
-const PRINT      = CUSTOM_CONFIG.print || true; // prints the file structure in the terminal
+const PRINT      = CUSTOM_CONFIG.print || false; // prints the file structure in the terminal
 const FILE_MODE  = process.argv[2]; // `ONLY` mode to only move the selected file types; `EXCLUDE` mode to move everything except the selected file types
 const FILE_TYPES = process.argv.slice(3); // list all file types separated by a single space, i.e. `.jpg .png .gif`
 
