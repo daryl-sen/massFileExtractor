6f1ca0c24b3d6710690727398df2e28fc679bff4
daryl <tsdaryl@gmail.com>
2021-04-08 20:56:49 -0700

    update gitignore

--- extract/index.js
+++ extract/index.js
@@ -1,9 +1,3 @@
 const { extract } = require('./mover');
 
-const SOURCE_DIR = 'target';
-const OUTPUT_DIR = 'collated';
-const PRINT      = true; // prints the file structure in the terminal
-const FILE_MODE  = process.argv[2]; // `ONLY` mode to only move the selected file types; `EXCLUDE` mode to move everything except the selected file types
-const FILE_TYPES = process.argv.slice(3); // list all file types separated by a single space, i.e. `.jpg .png .gif`
-
 extract();
--- extract/mover.js
+++ extract/mover.js
@@ -1,7 +1,17 @@
 const { readdirSync, lstatSync, rename, existsSync, mkdirSync } = require('fs');
 
+const SOURCE_DIR = 'target';
+const OUTPUT_DIR = 'collated';
+const PRINT      = true; // prints the file structure in the terminal
+const FILE_MODE  = process.argv[2]; // `ONLY` mode to only move the selected file types; `EXCLUDE` mode to move everything except the selected file types
+const FILE_TYPES = process.argv.slice(3); // list all file types separated by a single space, i.e. `.jpg .png .gif`
+
 const extract = (targetDir = SOURCE_DIR) => {
   // create the output directory
+  if (!existsSync(SOURCE_DIR)) {
+    mkdirSync(SOURCE_DIR);
+    console.log("We're setting up for the first time! Some instructions to come.");
+  }
   if (!existsSync(OUTPUT_DIR)) {
     mkdirSync(OUTPUT_DIR);
   }
